33af8491e23df31b73fc73471f9de9278b6d243d
Daniel Yu <46804746+yu-daniel@users.noreply.github.com>
2021-01-25 14:14:37 -0800

    initial Flask setup

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return "Hello Two Guys!"
+    return render_template("main.j2")
 
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 9112))
-    app.run(port=port)
+    app.run(port=port, debug=True)
