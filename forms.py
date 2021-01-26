3bac048eff70f5751e65cd1170a3abd2e9a391b0
Daniel Yu <46804746+yu-daniel@users.noreply.github.com>
2021-01-25 17:42:42 -0800

    merge fixes for forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
+from wtforms import StringField, IntegerField, DateField, BooleanField, DecimalField, SubmitField
 from wtforms.validators import DataRequired, NumberRange, Length
+
+class EmployeeManagerForm(FlaskForm):
+	first_name = StringField('First Name', validators=[DataRequired()])
+	last_name = StringField('Last Name', validators=[DataRequired()])
+	start_date = DateField('Start Date', validators=[DataRequired()])
+	status = BooleanField('Vacation', validators=[DataRequired()])
+	manager = BooleanField('Manager', validators=[DataRequired()])
+	managed_by = StringField('Managed by', validators=[DataRequired()])
+	store = StringField('Store', validators=[DataRequired()])
+	add = SubmitField('Add Employee/Manager')
+
+class LocationForm(FlaskForm):
+	city = StringField('City', validators=[DataRequired()])
+	state = StringField('State', validators=[DataRequired()])
+	zip_code = DecimalField('Zip Code', validators=[DataRequired()])
+	add = SubmitField('Add Location')
 
 class IngredientsForm(FlaskForm):
 	ingredient_id = IntegerField('Ingredient ID', validators = [DataRequired(), NumberRange(min=1, max=1000)])
