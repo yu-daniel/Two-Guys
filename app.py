01fd62bc24befed3988a87e2a3d61db4cead95ec
Daniel Yu <liquid.d9yu@gmail.com>
2021-01-25 19:55:03 -0800

    added suppliers in the ingredients-suppliers page, and created the orders-customers page.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, request
-from forms import EmployeeManagerForm, LocationForm, IngredientsForm
+from forms import EmployeeManagerForm, LocationForm, IngredientsForm, SuppliersForm, OrderForm, Customers
 
 app = Flask(__name__)
 
@@ -11,40 +11,80 @@ app.config['SECRET_KEY'] = '7cd4739b6ecbf78e2fb020b7f663a979'
 # dummy data
 headers = ['Name', 'Start Date', 'Vacation', 'Manager', 'Managed by', 'Location']
 data = [
-	['Alex Shin', '10/25/2011', 'Yes', 'No', 'Daniel Yu', 'LA'], 
-	['Daniel Yu', '11/11/2004', 'No', 'Yes', 'None', 'LA']
+    ['Alex Shin', '10/25/2011', 'Yes', 'No', 'Daniel Yu', 'LA'],
+    ['Daniel Yu', '11/11/2004', 'No', 'Yes', 'None', 'LA']
 ]
 
 # Just some dummy data for Step 3
-
-column_headers = ("ingredients_id", "order_date", "ingredient_name", "ingredient_cost", "order_id")
-sample_values = (
+ingredient_headers = ("ingredients_id", "order_date", "ingredient_name", "ingredient_cost", "order_id", "", "")
+ingredient_values = (
     (1, "2021-01-01", "ground beef", 10, 101),
     (2, "2021-01-02", "buns", 5, 102),
     (3, "2021-01-03", "tomatoes", 5, 103),
     (4, "2021-01-04", "onions", 2, 104),
     (5, "2021-01-05", "ground beef", 10, 105),
-    (6, "2021-01-06", "ketchip", 2, 106),
+    (6, "2021-01-06", "ketchup", 2, 106),
 )
+
+supplier_headers = ("supplier_id", "supplier_name", "", "")
+supplier_values = (
+    (100, "Johnson Ville"),
+    (101, "Meat Industry")
+)
+
+order_headers = ("order_id", "date_time", "sale_amount", "", "")
+order_values = (
+    (300, "2021-01-01", 900),
+    (301, "2021-01-02", 500),
+    (302, "2021-01-03", 340)
+)
+
+customer_headers = ("customer_id", "first_name", "last_name", "email", "phone_number", "", "")
+customer_values = (
+    (5000, "Daniel", "Yu", "danielyu@osu.com", "808-254-1999"),
+    (5001, "Alex", "Shin", "alexshin@osu.com", "702-153-0211")
+)
+
 
 # route for the homepage (root) is defined, but it's html is just the base
 @app.route("/", methods=['GET', 'POST'])
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route("/people", methods=['GET', 'POST'])
 def people():
-	emp_man_form = EmployeeManagerForm()
-	loc_form = LocationForm()
-	return render_template('emp_loc.html', headers=headers, data=data, emp_man_form=emp_man_form, loc_form=loc_form)
+    emp_man_form = EmployeeManagerForm()
+    loc_form = LocationForm()
+    return render_template('emp_loc.html', headers=headers, data=data, emp_man_form=emp_man_form, loc_form=loc_form)
+
 
 # route for the ingredients & suppliers page
 @app.route("/ingredients-suppliers", methods=["GET", "POST"])
 def ingredients_suppliers():
-    form = IngredientsForm()
-    return render_template("ingredients_suppliers.html", title='Register', form=form,
-                           column_headers=column_headers, sample_values=sample_values)
+    ingredient_form = IngredientsForm()
+    supplier_form = SuppliersForm()
+
+    return render_template("ingredients_suppliers.html", title='Register', ingredient_form=ingredient_form,
+                           column_headers=ingredient_headers, sample_values=ingredient_values,
+                           supplier_headers=supplier_headers, supplier_values=supplier_values,
+                           supplier_form=supplier_form
+                           )
+
+
+@app.route("/orders-customers", methods=["GET", "POST"])
+def orders_customers():
+    order_form = OrderForm()
+    customer_form = Customers()
+
+    return render_template("orders_customers.html", title='Register', order_form=order_form,
+                           customer_form=customer_form, order_headers=order_headers, order_values=order_values,
+                           customer_headers=customer_headers, customer_values=customer_values
+                           )
+
+
+
 
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT', 3001))
-	app.run(port=port)
+    port = int(os.environ.get('PORT', 3001))
+    app.run(port=port, debug=True)
--- forms.py
+++ forms.py
@@ -2,26 +2,51 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, BooleanField, DecimalField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Length, NumberRange
 
+
 class EmployeeManagerForm(FlaskForm):
-	first_name = StringField('First Name', validators=[DataRequired()])
-	last_name = StringField('Last Name', validators=[DataRequired()])
-	start_date = DateField('Start Date', validators=[DataRequired()])
-	status = BooleanField('Vacation', validators=[DataRequired()])
-	manager = BooleanField('Manager', validators=[DataRequired()])
-	managed_by = StringField('Managed by', validators=[DataRequired()])
-	store = StringField('Store', validators=[DataRequired()])
-	add = SubmitField('Add Employee/Manager')
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    start_date = DateField('Start Date', validators=[DataRequired()])
+    status = BooleanField('Vacation', validators=[DataRequired()])
+    manager = BooleanField('Manager', validators=[DataRequired()])
+    managed_by = StringField('Managed by', validators=[DataRequired()])
+    store = StringField('Store', validators=[DataRequired()])
+    add = SubmitField('Add Employee/Manager')
+
 
 class LocationForm(FlaskForm):
-	city = StringField('City', validators=[DataRequired()])
-	state = StringField('State', validators=[DataRequired()])
-	zip_code = DecimalField('Zip Code', validators=[DataRequired()])
-	add = SubmitField('Add Location')
+    city = StringField('City', validators=[DataRequired()])
+    state = StringField('State', validators=[DataRequired()])
+    zip_code = DecimalField('Zip Code', validators=[DataRequired()])
+    add = SubmitField('Add Location')
+
 
 class IngredientsForm(FlaskForm):
-	ingredient_id = IntegerField('Ingredient ID', validators = [DataRequired(), NumberRange(min=1, max=1000)])
-	order_date = IntegerField('Order Date', validators = [DataRequired(), NumberRange(min=1, max=10)])
-	ingredient_name = StringField('Ingredient Name', validators = [DataRequired(), Length(min=1, max=50)])
-	ingredient_cost = IntegerField('Ingredient ID', validators = [DataRequired()])
-	order_id = IntegerField('Order ID', validators = [DataRequired(), NumberRange(min=1, max=1000)])
-	submit = SubmitField('Submit')
+    ingredient_id = IntegerField('Ingredient ID', validators=[DataRequired(), NumberRange(min=1, max=1000)])
+    order_date = IntegerField('Order Date', validators=[DataRequired(), NumberRange(min=1, max=10)])
+    ingredient_name = StringField('Ingredient Name', validators=[DataRequired(), Length(min=1, max=50)])
+    ingredient_cost = IntegerField('Ingredient ID', validators=[DataRequired()])
+    order_id = IntegerField('Order ID', validators=[DataRequired(), NumberRange(min=1, max=1000)])
+    submit = SubmitField('Submit')
+
+
+class SuppliersForm(FlaskForm):
+    supplier_id = IntegerField('Supplier ID', validators=[DataRequired(), NumberRange(min=1, max=1000)])
+    supplier_name = StringField('Supplier Name', validators=[DataRequired(), Length(min=1, max=50)])
+    submit = SubmitField('Submit')
+
+
+class OrderForm(FlaskForm):
+    order_id = IntegerField('Ingredient ID', validators=[DataRequired(), NumberRange(min=1, max=1000)])
+    date_time = StringField('Date', validators=[DataRequired(), NumberRange(min=1, max=10)])
+    sale_amount = IntegerField('Sale Amount', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class Customers(FlaskForm):
+    customer_id = IntegerField('Customer ID', validators=[DataRequired(), NumberRange(min=1, max=1000)])
+    first_name = StringField('First Name', validators=[DataRequired(), Length(min=1, max=50)])
+    last_name = IntegerField('Last Name', validators=[DataRequired(), Length(min=1, max=50)])
+    email = StringField('Email', validators=[DataRequired()])
+    phone_number = IntegerField('Order ID', validators=[DataRequired(), NumberRange(min=1, max=20)])
+    submit = SubmitField('Submit')
