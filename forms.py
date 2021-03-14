72c8b5c8beba3267b0e93278525c06f7b665bd0f
ajss25 <alexshinjs@gmail.com>
2021-03-13 18:32:00 -0800

    Add more validators to forms.py

--- forms.py
+++ forms.py
@@ -1,32 +1,32 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, BooleanField, DecimalField, SubmitField, IntegerField, SelectField
-from wtforms.validators import DataRequired, Length, NumberRange
+from wtforms.validators import DataRequired, Length, NumberRange, Email
 
 
 class EmployeeManagerForm(FlaskForm):
-    first_name = StringField('First Name', validators=[DataRequired(), NumberRange(min=1, max=50)])
-    last_name = StringField('Last Name', validators=[DataRequired(), NumberRange(min=1, max=50)])
+    first_name = StringField('First Name', validators=[DataRequired(), Length(min=1, max=50)])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(min=1, max=50)])
     start_date = DateField('Start Date', validators=[DataRequired()], render_kw={"placeholder": "YYYY-MM-DD"})
     status = BooleanField('Vacation')
     manager = BooleanField('Manager')
-    managed_by = SelectField('Managed by', choices=['None'])
-    store = SelectField('Store')
+    managed_by = SelectField('Managed by', validators=[DataRequired()], choices=['None'])
+    store = SelectField('Store', validators=[DataRequired()])
     add = SubmitField('Add')
 
 
 class LocationForm(FlaskForm):
-    city = StringField('City', validators=[DataRequired(), NumberRange(min=2)])
-    state = StringField('State', validators=[DataRequired()])
+    city = StringField('City', validators=[DataRequired(), Length(min=2)])
+    state = StringField('State', validators=[DataRequired(), Length(min=2)])
     zip_code = DecimalField('Zip Code', validators=[DataRequired(), NumberRange(min=5, max=10)])
     add = SubmitField('Add')
 
 
 class IngredientsForm(FlaskForm):
-    order_date = StringField('Order Date', validators=[DataRequired(), Length(min=1, max=10)], render_kw={"placeholder": "YYYY-MM-DD"})
+    order_date = StringField('Order Date', validators=[DataRequired()], render_kw={"placeholder": "YYYY-MM-DD"})
     ingredient_name = StringField('Ingredient Name', validators=[DataRequired(), Length(min=1, max=50)])
-    ingredient_cost = IntegerField('Ingredient Cost', validators=[DataRequired()])
-    order_id = SelectField('Order ID')
-    supplier = SelectField('Supplier')
+    ingredient_cost = IntegerField('Ingredient Cost', validators=[DataRequired(), NumberRange(min=1)])
+    order_id = SelectField('Order ID', validators=[DataRequired()])
+    supplier = SelectField('Supplier', validators=[DataRequired()])
     submit = SubmitField('Add')
 
 
@@ -37,18 +37,18 @@ class SuppliersForm(FlaskForm):
 
 
 class OrderForm(FlaskForm):
-    date_time = StringField('Date', validators=[DataRequired(), NumberRange(min=1, max=10)], render_kw={"placeholder": "YYYY-MM-DD"})
-    sale_amount = IntegerField('Sale Amount', validators=[DataRequired()])
-    customer_id = SelectField('Customer ID')
+    date_time = StringField('Date', validators=[DataRequired()], render_kw={"placeholder": "YYYY-MM-DD"})
+    sale_amount = IntegerField('Sale Amount', validators=[DataRequired(), NumberRange(min=1)])
+    customer_id = SelectField('Customer ID', validators=[DataRequired()])
     submit = SubmitField('Add')
 
 
 class Customers(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired(), Length(min=1, max=50)])
     last_name = StringField('Last Name', validators=[DataRequired(), Length(min=1, max=50)])
-    email = StringField('Email', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     phone_number = IntegerField('Phone Number', validators=[DataRequired(), NumberRange(min=1, max=20)])
-    location = SelectField('Location')
+    location = SelectField('Location', validators=[DataRequired()])
     submit = SubmitField('Add')
 
 class SubmitCustomers(FlaskForm):
